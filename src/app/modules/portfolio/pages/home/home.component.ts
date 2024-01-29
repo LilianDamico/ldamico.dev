4b1b89e59a70e8e5718f5bd7bfe1635142f90969
LilianDamico <lfons@DESKTOP-552EQIA>
2024-01-29 16:54:04 -0300

    new

--- src/app/modules/portfolio/pages/home/home.component.ts
+++ src/app/modules/portfolio/pages/home/home.component.ts
@@ -3,11 +3,18 @@ import { HeaderComponent } from '../../components/header/header.component';
 import { KnowledgeComponent } from '../../components/knowledge/knowledge.component';
 import { ExperiencesComponent } from '../../components/experiences/experiences.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { ProjectsComponent } from '../../components/projects/projects.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, KnowledgeComponent, ExperiencesComponent, FooterComponent],
+  imports: [
+    HeaderComponent,
+    KnowledgeComponent,
+    ExperiencesComponent,
+    FooterComponent,
+    ProjectsComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
